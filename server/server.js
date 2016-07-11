d56a767e2c4c74902fdd4643ba22d9d77d43e446
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-11 12:15:07 -0700

    Initialize Mongo Database

--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
 var express = require('express');
 var app = express(); 
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var router = express.Router(); 
 var port = 8080;   
 
-router.use(function(req, res, next) {
-  console.log('Express server starting...');
-  next(); // make sure we go to the next routes and don't stop here
+//// Mongoose Initialization for Mongo 
+mongoose.connect('mongodb://localhost/movie-list'); // connect to our database
+var db = mongoose.connection;
+
+db.on('error', function(err) { // not necessary, but good practice 
+  console.error('Something went wrong...', err);
 });
 
+db.once('open', function() {
+  console.log('Mongo database is connected.');
+});
+
+//// Express Server Initialization 
+router.use(function(req, res, next) {
+  console.log('Express server starting...');
+  next(); 
+});
+
+app.use('/api', router);
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-  res.json({ message: 'hello world'});   
+router.get('/hello', function(req, res) {
+  res.send('hello world');   
 });
 
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Magic happening on port ' + port);
