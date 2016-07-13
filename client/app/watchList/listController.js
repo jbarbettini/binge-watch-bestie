810fe4ade871655a99352eea7df1dc1d7f23e3d3
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-12 17:39:08 -0700

    Clean up database of test data

--- client/app/watchList/listController.js
+++ client/app/watchList/listController.js
@@ -5,14 +5,5 @@ angular.module('bingeWatch.watchList', [])
   SearchService.getList().then(function(result) {
     console.log(result);
     $scope.listResults = result;
-
-    console.log('movie', result.image);
-    console.log('show', result.image);
-    console.log(result.type);
-    // if (result.artwork_304x171) {
-    //   $scope.image = result.artwork_304x171;
-    // } else {
-    //   $scope.image = result.poster_240x342;
-    // }
   });
 });
