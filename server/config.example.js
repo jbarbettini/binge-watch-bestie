2754e692b96163a3707e205811d2d080f2c79483
Jennifer Ong <jenn.r.ong@gmail.com>
2016-09-14 22:11:01 -0700

    Add config example file

---
+++ server/config.example.js
@@ -0,0 +1,8 @@
+// API keys are available for free at https://api.guidebox.com/production-key
+// Guidebox also provides a temporary API key for testing purposes that you can use 
+
+var API_KEY_SECRET = 'someLongStringOfCharacters'; 
+
+module.exports = {
+  apiKey: API_KEY_SECRET
+}; 
